226aaf163fe3b1b40ae12f74775bdbf7effddc53
GerDamn <34794685+GerDamn@users.noreply.github.com>
2017-12-23 16:42:54 +0100

    PeliculaDeGer
    Wingardium Leviosa, Gerr

---
+++ DSM5/Models/AssemblerPelicula.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SMPGenNHibernate.EN.SMP;
+
+
+namespace DSM5.Models
+{
+    public class AssemblerPelicula
+    {
+        public Pelicula ConvertENToModelUI(PeliculaEN en)
+        {
+            Pelicula pel = new Pelicula();
+            pel.id = en.Id;
+            pel.Nombre = en.Nombre;
+            pel.Valoracion = (int)en.Valoracion;
+      
+            return pel;
+
+
+        }
+        public IList<Pelicula> ConvertListENToModel(IList<PeliculaEN> ens)
+        {
+            IList<Pelicula> arts = new List<Pelicula>();
+            foreach (PeliculaEN en in ens)
+            {
+                arts.Add(ConvertENToModelUI(en));
+            }
+            return arts;
+        }
+    }
+}
---
+++ DSM5/Models/Pelicula.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DSM5.Models
+{
+    public class Pelicula
+    {
+        
+            [ScaffoldColumn(false)]
+            public int id { get; set; }
+
+
+
+
+            [Display(Prompt = "Nombre del Evento", Description = "Nombre de la Pelicula", Name = "Nombre ")]
+            [Required(ErrorMessage = "Debe indicar un nombre para la Pelicula")]
+            [StringLength(maximumLength: 200, ErrorMessage = "El nombre no puede tener más de 200 caracteres")]
+            public string Nombre { get; set; }
+
+
+            [Display(Prompt = "Valoracion de la Pelicula", Description = "Valoracion de la Pelicula", Name = "Valoracion ")]
+            [Required(ErrorMessage = "Debe indicar una valoración para la Pelicula")]
+            [Range(minimum: 1, maximum: 5, ErrorMessage = "El valoración debe ser mayor que cero y menor de 5")]
+            public int Valoracion { get; set; }
+
+
+  
+        
+    }
+}
